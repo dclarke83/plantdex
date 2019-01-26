fc4539cf01543a13ead1b974229018d85d7a5970
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-01-26 11:58:21 +0000

    Use sheet.js in plant list to load data for use

--- src/PlantList.js
+++ src/PlantList.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
+import config from './config';
+import load from './sheet';
 
 class PlantList extends Component {
+
+    state = {
+        loading: true,
+        error: null,
+        plants: []
+    };
+
+    componentDidMount() {
+        window.gapi.load('client', this.initClient);
+    }
+
+    onLoad = (data, error) => {
+        if (data) {
+            this.setState({
+                plants: data,
+                loading: false,
+            });
+        } else {
+            this.setState({ error, loading: false });
+        }
+    }
+
+    initClient = () => {
+        window.gapi.client
+            .init({
+                apiKey: config.apiKey,
+                discoveryDocs: config.discoveryDocs
+            })
+            .then(() => {
+                load(this.onLoad);
+            });
+    }
+
     render() {
         return (
             <div>
