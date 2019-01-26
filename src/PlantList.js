51394c17cf513c87752663decce9aa61789de188
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-01-26 11:36:56 +0000

    Add basic plantlist component

---
+++ src/PlantList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlantList extends Component {
+    render() {
+        return (
+            <div>
+                The plant list goes here
+            </div>
+        );
+    }
+}
+
+export default PlantList;
