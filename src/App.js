e7e234ed93b6cee71040080cb6d8d5dbef4216eb
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-01-29 12:43:56 +0000

    App changes to include header and filters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppHeader from './Header';
+import Filters from './Filters';
 import PlantList from './PlantList';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filtersVisible: false
+    };
+    this.ToggleFilters = this.ToggleFilters.bind(this);
+  }
+
+  ToggleFilters(e) {
+    this.setState(prevState => ({ filtersVisible: !prevState.filtersVisible }));
+  }
+
   render() {
     return (
       <div className="App">
+        <AppHeader onToggleFilters={this.ToggleFilters}></AppHeader>
+        <Filters filtersVisible={this.state.filtersVisible}></Filters>
         <PlantList />
       </div>
     );
