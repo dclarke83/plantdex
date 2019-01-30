490a52b8f7e5ac652527184029bc64868beddfe1
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-01-30 15:47:45 +0000

    Adding plants actiontypes

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,1 +1,7 @@
-export const TOGGLE_FILTERS = "TOGGLE_FILTERS";
+export const TOGGLE_FILTERS = 'TOGGLE_FILTERS';
+
+export const REQUEST_PLANTS = 'REQUEST_PLANTS';
+export const RECEIVE_PLANTS = 'RECEIVE_PLANTS';
+export const ERROR_PLANTS = 'ERROR_PLANTS';
+
+export const SET_SEARCH = 'SET_SEARCH';
