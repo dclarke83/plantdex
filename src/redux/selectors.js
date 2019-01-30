8b63c1c000ecb2535cc4414f9b35e235a7ca758a
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-01-30 15:48:40 +0000

    Adding plants selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,17 @@
+import { createSelector } from 'reselect';
+
 export const getFiltersState = store => store.filters;
+export const getPlantsState = store => store.plants;
 
 export const getFiltersVisibility = store => 
     getFiltersState(store) ? getFiltersState(store).filtersVisible : false;
+
+export const getSearchedPlants = createSelector(
+    [getPlantsState],
+    (state) => {
+        return state.plants.filter(plant => {
+            const regex = new RegExp(state.search, 'gi');
+            return plant.name.match(regex) || plant.commonName.match(regex);
+        });
+    }
+);
