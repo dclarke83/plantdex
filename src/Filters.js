567a3d4ec58aa5bf22d3aee59d45a0cb51fba002
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-02-01 15:59:13 +0000

    Styling the reset filters button

--- src/Filters.js
+++ src/Filters.js
@@ -28,6 +28,65 @@ const FilterElement = styled.div`
     margin-bottom: 15px;
 `;
 
+const StyledClearButton = styled.button`
+    margin: 0;
+    padding: 4px 8px;
+    min-width: 64px;
+    line-height: 1.75;
+    font-weight: 500;
+    text-transform: uppercase;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    border: 1px solid #cacaca;
+    border-radius: 4px;
+    position: relative;
+    user-select: none;
+    vertical-align: middle;
+    display: inline-flex;
+    outline: none;
+    align-items: center;
+    background-color: transparent;
+    cursor: pointer;
+    color: #4caf50;
+    font-family: 'Roboto';
+    letter-spacing: 0.02857em;
+
+    &:hover {
+        background-color: rgba(76, 175, 80, 0.2);
+        text-decoration:none;
+    }
+`;
+
+const ClearFiltersButton = (props) => {
+    const spacer = {
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        display: 'block',
+        zIndex: 0,
+        position: 'absolute',
+        overflow: 'hidden',
+        borderRadius: 'inherit',
+        pointerEvents: 'none',
+    };
+
+    const content = {
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+
+    return (
+        <StyledClearButton type='button' onClick={props.onClick}>
+            <span style={content}>
+                {props.text}
+            </span>
+            <span style={spacer}></span>
+        </StyledClearButton>
+    );
+}
+
 // Returns a new object with properties for each of
 // the filter names in the source array - for use
 // in setting up the internal state for react-select
@@ -120,7 +179,7 @@ class Filters extends Component {
                                 </div>
                             </FilterElement>
                         ))}  
-                        <button onClick={this.handleClear}>Clear</button>     
+                        <ClearFiltersButton text='Reset All' onClick={this.handleClear} />     
                     </SideBarContent>             
                 </SideBar>
             </div>
