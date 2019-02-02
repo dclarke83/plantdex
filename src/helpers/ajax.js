60d08bb21d321e8847500cf86dcbd1dab6e974da
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-02-02 21:49:54 +0000

    Adding some ajax helpers to reuse

---
+++ src/helpers/ajax.js
@@ -0,0 +1,35 @@
+export const post = ((url = '', data = {}) => {
+     // Default options are marked with *
+     return fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json()); // parses response to JSON 
+});
+
+export const put = ((url = '', data = {}) => {
+    // Default options are marked with *
+    return fetch(url, {
+       method: "PUT", // *GET, POST, PUT, DELETE, etc.
+       mode: "cors", // no-cors, cors, *same-origin
+       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+       credentials: "same-origin", // include, *same-origin, omit
+       headers: {
+           "Content-Type": "application/json",
+           // "Content-Type": "application/x-www-form-urlencoded",
+       },
+       redirect: "follow", // manual, *follow, error
+       referrer: "no-referrer", // no-referrer, *client
+       body: JSON.stringify(data), // body data type must match "Content-Type" header
+   })
+   .then(response => response.json()); // parses response to JSON 
+});
